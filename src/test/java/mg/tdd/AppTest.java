bd9f2ca2f40c9f4a533fc2ba6b4bba1906c2803b
Morten Granlund <morten.granlund@gmail.com>
2012-03-24 23:09:29 +0100

    Peanuts really...

--- src/test/java/mg/tdd/AppTest.java
+++ src/test/java/mg/tdd/AppTest.java
@@ -3,7 +3,13 @@
  */
 package mg.tdd;
 
+
 import static org.fest.assertions.Assertions.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+
+import java.util.List;
 
 import org.junit.Test;
 
@@ -15,6 +21,10 @@ public class AppTest {
 
 	@Test
 	public void shouldDoSomethingSmart() {
+		@SuppressWarnings("rawtypes")
+		List mock = mock(List.class);
+		
+		when(mock.get(anyInt())).thenReturn("element");
 		assertThat(new Boolean(true)).isNotNull();
 	}
 
