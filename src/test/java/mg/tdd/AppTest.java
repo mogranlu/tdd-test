91c46328d8ae61ea5d36dae8c0726ad44f2b4857
Morten Granlund <morten.granlund@gmail.com>
2012-03-24 22:56:53 +0100

    Added mockito 1.9 to root pom, and fixed compilation error in AppTest.java

--- src/test/java/mg/tdd/AppTest.java
+++ src/test/java/mg/tdd/AppTest.java
@@ -3,50 +3,19 @@
  */
 package mg.tdd;
 
+import static org.fest.assertions.Assertions.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
- * @author morten
- *
+ * @author Morten G.
+ * 
  */
 public class AppTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
 	public void shouldDoSomethingSmart() {
-		
+		assertThat(new Boolean(true)).isNotNull();
 	}
 
 }
