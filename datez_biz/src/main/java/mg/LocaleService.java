2f58c33e90e9d439d3307b5e81385cc5bc6ced45
Morten Granlund <morten.granlund@gmail.com>
2012-03-27 08:14:35 +0200

    Added a new spring project for biz

---
+++ datez_biz/src/main/java/mg/LocaleService.java
@@ -0,0 +1,12 @@
+package mg;
+
+import java.util.Locale;
+
+/**
+ * Hello world!
+ *
+ */
+public interface LocaleService 
+{
+    public String getSomethingFromSomething(Locale locale);
+}
---
+++ datez_biz/src/test/java/mg/LocaleServiceTest.java
@@ -0,0 +1,12 @@
+package mg;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class LocaleServiceTest 
+    extends TestCase
+{
+    // TODO: test some!
+}
